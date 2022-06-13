ca04d2fc8dde1bcf9916081cee04ac95f70fcb5d
rickifox <ricardo.pereira.developpeur@gmail.com>
2022-06-13 14:25:39 +0200

    DetailsNArounStaticPages

--- assets/scripts/addMap.js
+++ assets/scripts/addMap.js
@@ -1,3 +1,5 @@
+import '../styles/around.css';
+
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
@@ -5,6 +7,18 @@ import OSM from 'ol/source/OSM';
 import {fromLonLat} from "ol/proj";
 import Overlay from 'ol/Overlay';
 import {defaults} from 'ol/control';
+
+// const sectionsToToggle = [...document.getElementsByClassName('section-to-toggle')];
+const filters = [...document.querySelectorAll('.filter-icon')];
+
+document.getElementById('filters').addEventListener('click', (e) => {
+    console.log(e.target.id);
+    if(e.target.id !== "filters") {
+        filters.forEach(filter => {
+            filter.id === e.target.id ? filter.classList.add('chosen') : filter.classList.remove('chosen');
+        });
+    };
+});
 
 const mapGenerator = () => {
   const mapHolder = document.getElementById('map');
@@ -23,8 +37,11 @@ const mapGenerator = () => {
       zoom: 14
     }),
     controls: defaults({
+      rotate: false,
+      zoom: false,
       attributionOptions: {
-        collapsible: false
+        collapsible: true,
+        collapsed: true
       }
     })
   })
--- assets/scripts/sectionToggler.js
+++ assets/scripts/sectionToggler.js
@@ -1,8 +1,16 @@
+import '../styles/detailedView.css';
+
 const sectionsToToggle = [...document.getElementsByClassName('section-to-toggle')];
+const sectionSelectors = [...document.querySelectorAll('#visible-section-choices>li>button')];
 
 document.getElementById('visible-section-choices').addEventListener('click', (e) => {
-    console.log(sectionsToToggle);
-    sectionsToToggle.forEach(element => {
-        element.id === e.target.id + "-section" ? element.classList.add('visible') : element.classList.remove('visible');
-    });
+    console.log(e.target.id);
+    if(e.target.id !== "visible-section-choices") {
+        sectionsToToggle.forEach(element => {
+            element.id === e.target.id + "-section" ? element.classList.add('visible') : element.classList.remove('visible');
+        });
+        sectionSelectors.forEach(element => {
+            element.id === e.target.id ? element.classList.add('chosen') : element.classList.remove('chosen');
+        });
+    };
 });

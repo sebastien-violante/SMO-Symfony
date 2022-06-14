59d2dbbe98fbe07b99cec3a900045b18b462af07
sebastien-violante <sebastien.violante@gmail.com>
2022-06-14 17:29:34 +0200

    end of SMO...I hope

--- assets/responsive-slick.js
+++ assets/responsive-slick.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $('.slider').slick({
+  $('.newSlider').slick({
     dots: true,
     infinite: true,
     speed: 1000,
     centerMode: false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 2,
+    slidesToScroll: 2,
     responsive: [
       {
         breakpoint: 900,
@@ -22,14 +22,38 @@ $(document).ready(function(){
           slidesToShow: 1,
           slidesToScroll: 1,
           dots: true,
-          focusOnSelect: true
+          focusOnSelect: true,
+          centerMode: false,
         }
       }
     ]
   })
 
-  $('.around-logo').on('click', function() {
-    $('slider').slick('slickAdd', '<p>Hello !</p>')
+  $('.aroundSlider').slick({
+    infinite: true,
+    speed: 1000,
+    centerMode: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          focusOnSelect: true,
+          centerMode: true,
+        }
+      }
+    ]
   })
   
 });
